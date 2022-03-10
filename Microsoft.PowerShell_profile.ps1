a731382bd5af61d8a133a1fabf1896bac1f272b4
Mark Gutenberger <Mark-Gutenberger@outlook.com>
2022-03-09 21:24:54 -0600

    update `Wednesday, March 9, 2022 09:24:43 PM`

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,67 +4,62 @@ Author: Mark Gutenberger <mark-gutenberger@outlook.com>
 Microsoft.Powershell_profile.ps1 (c) 2022
 Desc: description
 Created:  2022-02-12T00:34:39.695Z
-Modified: 2022-03-09T23:27:27.468Z
+Modified: 2022-03-10T03:24:21.506Z
 #>
-
-# Clear this if you want, I just prefer to see that Powershell didn't shit its pants right away.
-Write-Host "pwsh " -NoNewline
-Get-Location | Write-Host
-
-# I hate that this is here, but for complete coverage, we need to unsure 1000% that is is ran
-
-function isOnWindows {
-	# This will work on 6.0 and later but is missing on
-	# older versions
-	if (Test-Path -Path 'variable:global:IsWindows') {
-		return Get-Content -Path 'variable:global:IsWindows'
+<# *
+   * Self explanatory...
+   * #>
+function Get-Platform {
+	function Test-Platform-Windows {
+		# This will work on 6.0 and later but is missing on
+		# older versions
+		if (Test-Path -Path 'variable:global:IsWindows') {
+			return Get-Content -Path 'variable:global:IsWindows'
+		}
+		# This should catch older versions
+		elseif (Test-Path -Path 'env:os') {
+			return (Get-Content -Path 'env:os').StartsWith("Windows")
+		}
+		# If all else fails
+		else {
+			return $false
+		}
 	}
-	# This should catch older versions
-	elseif (Test-Path -Path 'env:os') {
-		return (Get-Content -Path 'env:os').StartsWith("Windows")
-	}
-	# If all else fails
-	else {
+
+	function Test-Platform-Linux {
+		if (Test-Path -Path 'variable:global:IsLinux') {
+			return Get-Content -Path 'variable:global:IsLinux'
+		}
+
 		return $false
 	}
-}
 
-function isOnLinux {
-	if (Test-Path -Path 'variable:global:IsLinux') {
-		return Get-Content -Path 'variable:global:IsLinux'
+	function Test-Platform-Darwin {
+		# The variable to test if you are on Mac OS changed from
+		# IsOSX to IsMacOS. Because I have Set-StrictMode -Version Latest
+		# trying to access a variable that is not set will crash.
+		# So I use Test-Path to determine which exist and which to use.
+		if (Test-Path -Path 'variable:global:IsMacOS') {
+			return Get-Content -Path 'variable:global:IsMacOS'
+		}
+		elseif (Test-Path -Path 'variable:global:IsOSX') {
+			return Get-Content -Path 'variable:global:IsOSX'
+		}
+		else {
+			return $false
+		}
 	}
-
-	return $false
-}
-
-function isOnMac {
-	# The variable to test if you are on Mac OS changed from
-	# IsOSX to IsMacOS. Because I have Set-StrictMode -Version Latest
-	# trying to access a variable that is not set will crash.
-	# So I use Test-Path to determine which exist and which to use.
-	if (Test-Path -Path 'variable:global:IsMacOS') {
-		return Get-Content -Path 'variable:global:IsMacOS'
-	}
-	elseif (Test-Path -Path 'variable:global:IsOSX') {
-		return Get-Content -Path 'variable:global:IsOSX'
-	}
-	else {
-		return $false
-	}
-}
-
-function Is-OnPlatform {
-	if (isOnWindows) {
+	if (Test-Platform-Windows) {
 		$env:IsOnPlatform = 'Windows'
 		$env:IsOnPlatform0 = '0'
 		return 'Windows'
 	}
-	if (isOnLinux) {
+	if (Test-Platform-Linux) {
 		$env:IsOnPlatform = 'Linux'
 		$env:IsOnPlatform0 = '1'
 		return 'Linux'
 	}
-	if (isOnMac) {
+	if (Test-Platform-Darwin) {
 		$env:IsOnPlatform = 'Mac'
 		$env:IsOnPlatform0 = '2'
 		return 'Mac'
@@ -73,88 +68,63 @@ function Is-OnPlatform {
 	$env:IsOnPlatform0 = '3'
 	return 'You have fucked up! 👏'
 }
+function Import-External-Scripts () {
 
-Write-Host "Platform: " -NoNewline
-Is-OnPlatform
+	if ($env:IsOnPlatform -eq 'Windows') {
+		# Windows version of this shit :D
+		# Go here
+		Set-Location -Path $env:userprofile\Documents\PowerShell\
+		# Dot in external modules
+		. .\PSFormat\Main.ps1
 
-if ($env:IsOnPlatform -eq 'Windows') {
-	# Windows version of this shit :D
-	# Go here
-	Set-Location -Path $env:userprofile\Documents\PowerShell\
-	# Dot in external modules
-	. .\Is-OnPlatform\Main.ps1
-	. .\ls\Main.ps1
-	. .\PSFormat\Main.ps1
-
-}
-elseif ($env:IsOnPlatform -eq 'Linux') {
-	# Linux version of this shit :D
-	# Go here
-	Set-Location ~/.config/powershell/
-	# Dot in external modules
-	. ./Is-OnPlatform/Main.ps1
-	. ./ls/Main.ps1
-	. ./PSFormat/Main.ps1
-
-}
-elseif ($env:IsOnPlatform -eq 'Mac') {
-	# Mac version of this shit :D
-	# Go here
-	Set-Location ~/.config/powershell/
-	# Dot in external modules
-	. ./Is-OnPlatform/Main.ps1
-	. ./ls/Main.ps1
-	. ./PSFormat/Main.ps1
-}
-else {
-	Write-Host "You have fucked up! 👏"
-	return $false
-	exit 0;
-}
-
-# again, just check to see if PS is shitting bricks
-Write-Host "Found profile: " -NoNewline
-Get-Location | Write-Host
-
-function Custom-wsl {
-	if ($args.Count -lt 1) {
-		C:\\Windows\\System32\\wsl.exe bash
 	}
-	elseif ($args.Contains('ubuntu')) {
-		C:\\Windows\\System32\\wsl.exe $args bash
+	elseif ($env:IsOnPlatform -eq 'Linux') {
+		# Linux version of this shit :D
+		# Go here
+		Set-Location ~/.config/powershell/
+		# Dot in external modules
+		. ./PSFormat/Main.ps1
+
 	}
-	elseif ($args.Contains('debian')) {
-		C:\\Windows\\System32\\wsl.exe $args
+	elseif ($env:IsOnPlatform -eq 'Mac') {
+		# Mac version of this shit :D
+		# Go here
+		Set-Location ~/.config/powershell/
+		# Dot in external modules
+		. ./PSFormat/Main.ps1
 	}
 	else {
-		C:\\Windows\\System32\\wsl.exe $args
+		Write-Host "You have fucked up! 👏"
+		return $false
+		exit 0;
 	}
 };
-
-function Create-Empty-Commit () {
-	git commit --allow-empty -m "Empty commit"
-}
-
-Set-Alias wsl Custom-wsl;
-Set-Alias bash wsl;
+<# *
+   * Alias declarations:
+   * #>
 Set-Alias rn React-Native;
-Set-Alias ls Custom-ls;
-Set-Alias list Custom-ls;
-Set-Alias gitempty Create-Empty-Commit;
 Set-Alias PSFormat PS-Format;
 
-# custom prompt:
-# function Prompt() {
-# "PS $($executionContext.SessionState.Path.CurrentLocation)$( '>' * ($nestedPromptLevel + 1)) ";
-# };
+<# *
+   * Starship stuff
+   * #>
 function Invoke-Starship-PreCommand {
 	$host.UI.Write("`e]0;$pwd`a")
 };
-
 Invoke-Starship-PreCommand
-
 Invoke-Expression (& starship init powershell)
 
-# At the very end make sure to cd back to home. (no args needed as home is teh defualt param)
-
-Set-Location
+<# *
+   * Main
+   * #>
+function Main () {
+	Import-External-Scripts
+	Write-Host "pwsh " -NoNewline
+	Get-Location | Write-Host
+	Write-Host "Platform: " -NoNewline
+	Get-Platform
+	Write-Host "Found profile: " -NoNewline
+	Get-Location | Write-Host
+	Set-Location
+};
+Main
--- PSFormat/Main.ps1
+++ PSFormat/Main.ps1
@@ -1,5 +1,3 @@
-Get-ChildItem -Path .\ -Include *.ps1,*.psm1 -Recurse | Edit-DTWBeautifyScript -IndentType Tabs
-
 function PS-Format () {
 	Get-ChildItem -Path .\ -Include *.ps1,*.psm1 -Recurse | Edit-DTWBeautifyScript -IndentType Tabs
 };
