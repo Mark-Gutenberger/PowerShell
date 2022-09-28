b7b1e8c44871e6884e4333f20e8a85c1e5abdb66
Mark Gutenberger <gutenfries@gmail.com>
2022-09-28 11:55:24 -0500

    improve starship config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,30 +3,34 @@ Import-Module -Name CompletionPredictor
 
 $env:PYTHONIOENCODING = "utf-8"
 
-function Invoke-Admin () {
-	if ($Args.Count -eq 0) {
-		return $null
-	}
- elseif ($Args.Count -ge 2) {
-		return $null
-	}
- else {
-		# convert args to string
-		$args_ = $Args[0].ToString()
-		try {
-			Start-Process $args_ -Verb runAs
-		}
-		catch {
-			Write-Host  "Error: No batch operation, program, or executable matching the pattern of ``$args_`` found...`nEnsure the path is correct or that it is accessible from your PATH variable." -ForegroundColor Red
-			return $null
-		};
-	};
+function Invoke-Admin ()
+{
+  if ($Args.Count -eq 0)
+  {
+    return $null
+  } elseif ($Args.Count -ge 2)
+  {
+    return $null
+  } else
+  {
+    # convert args to string
+    $args_ = $Args[0].ToString()
+    try
+    {
+      Start-Process $args_ -Verb runAs
+    } catch
+    {
+      Write-Host  "Error: No batch operation, program, or executable matching the pattern of ``$args_`` found...`nEnsure the path is correct or that it is accessible from your PATH variable." -ForegroundColor Red
+      return $null
+    };
+  };
 };
 
 Set-Alias Admin Invoke-Admin
 
-function Invoke-Batstat () {
-	WMIC PATH Win32_Battery Get EstimatedChargeRemaining
+function Invoke-Batstat ()
+{
+  WMIC PATH Win32_Battery Get EstimatedChargeRemaining
 };
 
 Set-Alias Batstat Invoke-Batstat
@@ -34,17 +38,19 @@ Set-Alias Battery Invoke-Batstat
 
 Set-Alias WinTerm wt
 
-function Invoke-ls() {
-	# wrapper for ls command
-	$ls = "C:\Users\Mark-\source\lsd\target\debug\lsd.exe"
-	# run lsd
-	try {
-		& $ls -a $Args
-	}
- catch {
-		Write-Host "Error: No batch operation, program, or executable matching the pattern of ``$ls`` found...`n Please edit your powershell profile and update the variable" -ForegroundColor Yellow
-		Get-ChildItem $Args
-	}
+function Invoke-ls()
+{
+  # wrapper for ls command
+  $ls = "C:\Users\Mark-\source\lsd\target\debug\lsd.exe"
+  # run lsd
+  try
+  {
+    & $ls -a $Args
+  } catch
+  {
+    Write-Host "Error: No batch operation, program, or executable matching the pattern of ``$ls`` found...`n Please edit your powershell profile and update the variable" -ForegroundColor Yellow
+    Get-ChildItem $Args
+  }
 };
 Set-Alias ls Invoke-ls
 
@@ -65,40 +71,42 @@ Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 # `ForwardChar` accepts the entire suggestion text when the cursor is at the end of the line.
 # This custom binding makes `RightArrow` behave similarly - accepting the next word instead of the entire suggestion text.
-Set-PSReadLineKeyHandler -Key RightArrow `
-	-BriefDescription ForwardCharAndAcceptNextSuggestionWord `
-	-LongDescription "Move cursor one character to the right in the current editing line and accept the next word in suggestion when it's at the end of current editing line" `
-	-ScriptBlock {
-	param($key, $arg)
+Set-PSReadLineKeyHandler -Key Shift+RightArrow `
+  -BriefDescription ForwardCharAndAcceptNextSuggestionWord `
+  -LongDescription "Move cursor one character to the right in the current editing line and accept the next word in suggestion when it's at the end of current editing line" `
+  -ScriptBlock {
+  param($key, $arg)
 
-	$line = $null
-	$cursor = $null
-	[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+  $line = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
 
-	if ($cursor -lt $line.Length) {
-		[Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
-	}
- else {
-		[Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg)
-	}
+  if ($cursor -lt $line.Length)
+  {
+    [Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
+  } else
+  {
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg)
+  }
 }
 
-Set-PSReadLineKeyHandler -Key Tab `
-	-BriefDescription ForwardCharAndAcceptNextSuggestionWord `
-	-LongDescription "Move cursor one character to the right in the current editing line and accept the next word in suggestion when it's at the end of current editing line" `
-	-ScriptBlock {
-	param($key, $arg)
+Set-PSReadLineKeyHandler -Key Shift+Tab `
+  -BriefDescription ForwardCharAndAcceptNextSuggestionWord `
+  -LongDescription "Move cursor one character to the right in the current editing line and accept the next word in suggestion when it's at the end of current editing line" `
+  -ScriptBlock {
+  param($key, $arg)
 
-	$line = $null
-	$cursor = $null
-	[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+  $line = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
 
-	if ($cursor -lt $line.Length) {
-		[Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
-	}
- else {
-		[Microsoft.PowerShell.PSConsoleReadLine]::AcceptSuggestion($key, $arg)
-	}
+  if ($cursor -lt $line.Length)
+  {
+    [Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
+  } else
+  {
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptSuggestion($key, $arg)
+  }
 }
 
 Set-Alias lvim "C:\Users\Mark-\documents\powershell\lvim\lvim.ps1"
